427255aba70a229e74648b67aec068cb522d34f4
Darcy Jones <darcy.ab.jones@gmail.com>
2019-12-15 23:15:50 +0800

    rename from pafpal to paf.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-//use pafpal::parser;
 use std::str::FromStr;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let input = "seqid\t10\t0\t10\t+\tseqid2\t10\t0\t10\t1\t1\t1000\n";
-    match pafpal::PAF::from_str(input) {
+    match paf::PAF::from_str(input) {
         Ok(p) => println!("{}", p),
         Err(e) => println!("{}", e),
     };
